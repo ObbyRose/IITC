d2cea9c85efe56585027f9b7997d64fee06d7323
Tal Maman <dreapet41s@gmail.com>
2024-07-23 16:26:12 +0300

    init

---
+++ 2024/JULY/23.7/task01.js
@@ -0,0 +1,32 @@
+// let firstName = "Tal"
+// let lastName = "Maman"
+// let fullName= firstName+ " " + lastName
+// let age = 22 
+// const isStudent= true
+// const isAdult=age>18
+// const isJohn=false
+// if(fullName && isStudent && isAdult)
+// console.log(`${fullName} is an adult student`)
+
+// else{
+//     console.log(fullName, "is not a student")
+// }
+
+// if(isJohn===true){
+// console.log("His name is John")
+// }
+// else{
+//     console.log("His name is not John")
+// }
+
+
+//TASK 02
+// let firstName="Tal"
+// let lastName="Maman"
+// function greet (firstName, lastName){
+// fullName=`${firstName} ${lastName}`
+// console.log(`Hi ${fullName.toUpperCase()} Welcome to the IITC College`)
+// }
+// greet(`Tal`, `Maman`)
+
+//Task 03
