0809c5f28a78fb9d82f174032f525d28609baaa4
Tal Maman <dreapet41s@gmail.com>
2024-07-24 11:37:50 +0300

    innit

--- 2024/JULY/23.7/task01.js
+++ 2024/JULY/23.7/task01.js
@@ -160,6 +160,76 @@
 //         return "invalid username or password"
 //     }
 // }
-// console.log(validateLogin("Dobby", "1234"));
-// console.log(validateLogin("Dobby", "123"));
-// console.log(validateLogin("Doy", "1234"));
+// console.log(validateLogin("Dobby", "1234")); // Login Successful
+// console.log(validateLogin("Dobby", "123")); // invalid username or password
+// console.log(validateLogin("Doy", "1234")); // invalid username or password
+
+// Initials------------------------------
+
+// function extractInitials(firstName, lastName){
+
+//     let firstInitial=firstName.charAt(0).toUpperCase();
+//     let secondInitial=lastName.charAt(0).toUpperCase();
+//     let initials=firstInitial+`.`+secondInitial;
+//     return initials;
+// }
+// console.log(extractInitials("Tal", "Maman")); // T.M
+// console.log(extractInitials("Dobby", "TheElf")); // D.T
+
+//string replacement-----------------
+// function maskEmail(email){
+//     let masking= email.split(`@`)
+//     let maskedUsername = masking[0].replace(/./g, '*');
+//     let maskedEmail=maskedUsername + "@" + masking[1]
+
+//     return maskedEmail;
+// }
+// console.log(maskEmail(`Dreapet41s@gmail.com`)) // *********@gmail.com
+
+
+// function gradeCalculator(score){
+//     let studentGrade=score
+//     if (studentGrade > 90) return "A";
+//     else if (studentGrade > 80) return "B"; 
+//     else if (studentGrade > 70) return "C";
+//     else if (studentGrade > 60) return "D";
+//     else return "F";
+// }
+// console.log(gradeCalculator(91)) // A
+// console.log(gradeCalculator(81)) // B
+// console.log(gradeCalculator(71)) // C
+// console.log(gradeCalculator(61)) // D
+// console.log(gradeCalculator(51)) // F
+
+//complex boolean--------------------------------------------
+// function canVote (age, isCitizen){
+//     if (age>=18 && isCitizen===true) return "You are eligible to vote!"
+//     else return "You are not eligible to vote";
+// }
+// console.log(canVote(18, true)) // You are eligible to vote!
+// console.log(canVote(18, false)) // You are not eligible to vote
+// console.log(canVote(10, true)) // You are not eligible to vote
+
+// String and Number Conversion:--------------------------
+// function convertToUpperCaseAndAddAge(name, age){
+// let capitalizedName=name.toUpperCase()
+// let addAge=capitalizedName+age; 
+// return addAge;
+
+// }
+// console.log(convertToUpperCaseAndAddAge("tal", 12)) // TAL12
+
+// Capitalize First Letter:----------------------------------
+// function capitalized(word){
+//     let capitalizedWord=word.charAt(0).toUpperCase() + word.substring (1)
+//     return capitalizedWord;
+// }
+// console.log(capitalized("Rabies")) // returns Rabies
+
+//Check Substring:--------------------------------------------
+    // function containsSubstring(mainString, subString){
+    //     return (mainString.includes(subString))
+    // }
+    // console.log(containsSubstring("Johnny had a wild ride", "i")) // true
+    // console.log(containsSubstring("Johnny had a wild ride", "riders")) // false
+    // console.log(containsSubstring("Johnny had a wild ride", "wild")) // true
