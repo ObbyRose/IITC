403a9a553b6b09db2fa24cebc269e31593c44213
Tal Maman <dreapet41s@gmail.com>
2024-07-25 14:41:11 +0300

    homework part 2

---
+++ 2024/JULY/25.7/Weekend HW/main3.js
@@ -0,0 +1,135 @@
+// # JavaScript Comparison Operators Quiz
+
+// ## Introduction
+// Before starting the quiz, watch this video for an overview of JavaScript comparison operators:
+// [JavaScript Comparison Operators](https://www.youtube.com/watch?v=KwOTbF1kEcU)
+
+// ## Questions
+
+// 1. What is the equality operator in JavaScript?
+//    Hint: There are two types of equality operators in JavaScript.
+
+// let a= 5
+// let b=5
+// if (a==b) console.log("they are similar");
+// let c = 8
+// let d = 8
+// if (c===d) console.log("they are similar");
+
+//    [Learn more about equality operators](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 2. What's the difference between == and ===?
+//    Hint: One checks for equality without type conversion, the other doesn't.
+//
+// A:One checks for equality without type conversion, the other doesn't.
+
+//    [Learn more about strict equality](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 3. How do you check if two values are not equal in JavaScript?
+//    Hint: There are two operators for this, similar to the equality operators.
+
+// let a=5
+// let b=10
+// if(a!==b) console.log("They're different");
+
+//    [Learn more about inequality operators](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 4. What does the > operator do?
+//    Hint: It compares two values in a specific way.
+
+// a:It compares two values in a specific way.
+   
+//    [Learn more about comparison operators](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 5. What does the < operator do?
+//    Hint: It's the opposite of the > operator.
+
+//a: It's the opposite of the > operator. and still compares two values in a specific way
+
+//    [Learn more about comparison operators](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 6. How do you check if a value is greater than or equal to another value?
+//    Hint: It's a combination of two basic comparison operators.
+
+   //  let a=5
+   //  let b=6
+   //  if(a>=b) console.log(true); else console.log(false);
+
+//    [Learn more about comparison operators](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 7. How do you check if a value is less than or equal to another value?
+//    Hint: It's similar to the previous question, but in the opposite direction.
+
+   //  let a=5
+   //  let b=6
+   //  if(a<=b) console.log(true); else console.log(false);
+
+//    [Learn more about comparison operators](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 8. What is the result of 5 > 3?
+//    Hint: Evaluate the comparison based on the numbers' values.
+//
+// let a= 5
+// let b = 3
+// if (a>b) console.log(true) // it will indeed return true but 5 is indeed 5>3
+//    [Learn more about comparison operators](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 9. What is the result of 10 === "10"?
+//    Hint: Remember what === does with types.
+// let a="10"
+// a===10
+// console.log(a) // the result would be an equal to 10
+//    [Learn more about strict equality](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 10. What is the result of 7 <= 7?
+//     Hint: Consider what "less than or equal to" means.
+
+// the result would be true as it says less then or equal and it is indeed equal
+
+//     [Learn more about comparison operators](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 11. How would you compare two strings alphabetically?
+//     Hint: You can use the same operators as you would for numbers.
+// let a="a"
+// let b="a"
+// if(a===b) console.log(true); else console.log(false);
+//     [Learn more about string comparison](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 12. What's the result of "apple" < "banana"?
+//     Hint: Strings are compared character by character.
+
+   //   let string1="apple"
+   //   let string2="banana"
+   //   if(string1>string2) console.log("they are not in alphabetical order");
+   //   else console.log("They are in alphabetical order");
+
+//     [Learn more about string comparison](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 13. What does the !== operator do?
+//     Hint: It's related to the === operator.
+//
+// A: it is the exact opposite of === and what it defines is that it checks that the value or the string is not similar 
+// for example: let a=5
+               //  let b =6
+// if we will check if (a!==b) it will return as true
+
+//     [Learn more about strict inequality](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 14. What's the result of 5 !== "5"?
+//     Hint: Consider both the value and type of the operands.
+
+// the result would be true due because the value is not similar to a string
+// console.log(5!=="5");
+
+//     [Learn more about strict inequality](https://www.w3schools.com/js/js_comparisons.asp)
+
+// 15. How do comparison operators work with different data types?
+//     Hint: JavaScript may perform type coercion in some cases.
+// 
+// A: JavaScript variables can be converted to a new variable and another data type:
+
+//By the use of a JavaScript function OR Automatically by JavaScript itself
+//
+//     [Learn more about type coercion in comparisons](https://www.w3schools.com/js/js_type_conversion.asp)
+
+// Remember to test your knowledge by trying these operations in a JavaScript console!
