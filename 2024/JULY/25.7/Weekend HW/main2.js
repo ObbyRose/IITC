08ba0025ca133ba11f06fd48f268e0f868fe2a4d
Tal Maman <dreapet41s@gmail.com>
2024-07-25 12:35:07 +0300

    Homework P1

---
+++ 2024/JULY/25.7/Weekend HW/main2.js
@@ -0,0 +1,141 @@
+// # JavaScript Assignment Operators Quiz
+
+// ## Introduction
+// Before starting the quiz, watch this video for an overview of JavaScript assignment operators:
+// [JavaScript Assignment Operators](https://www.youtube.com/watch?v=h9t8r25oPqs)
+
+// ## Questions
+
+// 1. What is the basic assignment operator in JavaScript?
+//    Hint: It's the most commonly used operator for assigning values.
+
+      // A:
+      //let a = `=` // the assignment of `=`
+
+//    [Learn more about assignment operators](https://www.w3schools.com/js/js_assignment.asp)
+
+// 2. How do you assign a value to a variable in JavaScript?
+//    Hint: Use the basic assignment operator.
+
+   // A:
+     // let a=5;
+
+//    [Learn more about variable assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 3. What does the += operator do?
+//    Hint: It combines addition with assignment.
+
+      // A:
+      //It combines addition with assignment.
+
+
+//    [Learn more about addition assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 4. How would you add 5 to a variable x using +=?
+//    Hint: Use the += operator with the value 5.
+
+//    A:
+      // let x=8
+      //  x+=5
+      // console.log(x);
+       
+//    [Learn more about addition assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 5. What does the -= operator do?
+//    Hint: It combines subtraction with assignment.
+
+      //A:
+      // It combines subtraction with assignment.
+
+//    [Learn more about subtraction assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 6. How would you subtract 3 from a variable y using -=?
+//    Hint: Use the -= operator with the value 3.
+
+//    A:
+      // let y = 5
+      // y-=3
+      // console.log(y);
+
+//    [Learn more about subtraction assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 7. What does the *= operator do?
+//    Hint: It combines multiplication with assignment.
+
+// A: It combines multiplication with assignment.
+
+//    [Learn more about multiplication assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 8. How would you multiply a variable z by 2 using *=?
+//    Hint: Use the *= operator with the value 2.
+
+// A:
+      // let z = 11
+      // z*=2
+      // console.log(z);
+
+//    [Learn more about multiplication assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 9. What does the /= operator do?
+//    Hint: It combines division with assignment.
+
+// A: It combines division with assignment
+
+//    [Learn more about division assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 10. How would you divide a variable w by 4 using /=?
+//     Hint: Use the /= operator with the value 4.
+// A:
+      //  let w=12
+      //  w/=4
+      //  console.log(w);       
+
+//     [Learn more about division assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// 11. What's the difference between x = x + 1 and x += 1?
+//     Hint: They achieve the same result, but one is more concise.
+
+// A: They achieve the same result, but one is more concise.
+
+//     [Learn more about increment operators](https://www.w3schools.com/js/js_assignment.asp)
+
+// 12. How do you increase a variable by 1 using an assignment operator?
+//     Hint: There are multiple ways, including using +=.
+
+// A:
+      // let c=5
+      // c+=1
+      // console.log(c);
+
+//     [Learn more about increment operators](https://www.w3schools.com/js/js_assignment.asp)
+
+// 13. What happens when you use += with strings?
+//     Hint: Think about how strings can be combined.
+
+
+      //  let a ="change"
+      //  a+="raise"
+      //  console.log(a);
+      // All that would be done is the addition of the value or the string next to the original variable
+
+//     [Learn more about string operators](https://www.w3schools.com/js/js_operators.asp)
+
+// 14. How would you append the string "Hello" to a variable str using +=?
+//     Hint: Use the += operator with the string "Hello".
+
+      //  let str ="richard said"
+         // str+= " Hello"
+         // console.log(str);
+
+//     [Learn more about string operators](https://www.w3schools.com/js/js_operators.asp)
+
+// 15. What's the result of let x = 5; x += 3;?
+//     Hint: First assign 5 to x, then add 3 to it.
+       
+      //  let x = 5
+      //  x+=3
+      //  console.log(x) // the answer would be 8
+
+//     [Learn more about addition assignment](https://www.w3schools.com/js/js_assignment.asp)
+
+// Remember to test your knowledge by trying these operations in a JavaScript console!
