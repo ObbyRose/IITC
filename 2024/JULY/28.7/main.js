c9d725b554adb578f342f5f82caf88ff58d1a172
Tal Maman <dreapet41s@gmail.com>
2024-07-28 13:21:08 +0300

    class

---
+++ 2024/JULY/28.7/main.js
@@ -0,0 +1,25 @@
+
+// CLASS WORK
+
+// for(let i=10; i>0; i--){
+//     console.log(`The current number is ${i}`)
+// }
+//-----------------------------------
+// for (let i=0; i<22; i+=2){
+//     console.log(`Even numbers ${i}`);
+// }
+//-----------------------------------
+// let sum=0
+// for(i=1; i<=10; i++){
+//     sum=i+sum
+// }
+// console.log(sum);
+//-----------------------------------
+// for(let i=0; i<=10; i++){
+// let multiply=i*5
+// console.log(multiply);
+// }
+//-----------------------------------
+// for (i=0; i<=5; i++){
+// console.log(`*`.repeat(i));
+// }
