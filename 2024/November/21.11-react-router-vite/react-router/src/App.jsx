4235bfa5221a7d56e9531e4d23ea065eed640edd
Tal Maman <dreapet41s@gmail.com>
2024-11-21 10:37:40 +0200

    .

---
+++ 2024/November/21.11-react-router-vite/react-router/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css'
+
+//Page Import
+import About from "../pages/About/About.jsx"
+import Home from "../pages/Home/Home.jsx"
+import Dashboard from "../pages/Dashboard/Dashboard.jsx"
+import Articles from "../pages/Articles/Articles.jsx"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/about" element = {<About/>}/>
+        <Route path="/dashboard" element = {<Dashboard/>}/>
+        <Route path="/articles" element = {<Articles/>}>
+          <Route path="news" element={<h2>This is News section</h2>}/>
+          <Route path="politics" element={<h2>This is Politics section</h2>}/>
+          <Route path="tech" element={<h2>This is Tech section</h2>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
